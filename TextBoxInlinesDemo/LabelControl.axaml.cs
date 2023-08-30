e3fc0aabe3d67275650483640ec7334e4b1131a7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-08-30 11:24:41 +0200

    Add LabelControl

---
+++ TextBoxInlinesDemo/LabelControl.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace TextBoxInlinesDemo;
+
+public class LabelControl : TemplatedControl
+{
+    public static readonly StyledProperty<string?> TextProperty = 
+        AvaloniaProperty.Register<LabelControl, string?>(nameof(Text));
+
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
+
